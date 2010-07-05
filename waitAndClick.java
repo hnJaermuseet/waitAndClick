001435bc2af4148ba617dc7939818f598e5c4bc5
hnJaermuseet <hn@jaermuseet.no>
2010-07-05 13:33:43 +0200

    Version 1

---
+++ waitAndClick.java
@@ -0,0 +1,35 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+import sun.awt.windows.ThemeReader;
+
+
+public class waitAndClick {
+
+	/**
+	 * Waits 10 seconds and clicks first mouse button in 0,0
+	 * 
+	 * @author Hallvard Nygard, hn@jaermuseet.no
+	 * @see 
+	 * @param args
+	 * @throws AWTException 
+	 */
+	public static void main(String[] args) throws AWTException {
+		try {
+			Thread.sleep(10000); // 10 seconds
+		} catch (InterruptedException e) {
+			
+		}
+		
+		Robot rob = new Robot();
+		rob.delay(500);
+		rob.mouseMove(0, 0);
+		rob.delay(500);
+		rob.mousePress(InputEvent.BUTTON1_MASK);
+		rob.delay(100);
+		rob.mouseRelease(InputEvent.BUTTON1_MASK);
+		
+		System.exit(1);
+	}
+}
